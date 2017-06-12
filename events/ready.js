af8095712baa8797a9126a2fc4d3c409b1296025
Adam Mahood <mahood.adam@gmail.com>
2017-06-12 16:19:02 -0400

    Various Updates

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,11 @@
 exports.run = (client) => {
   const pkg = require("../package.json");
+  const settings = require("../settings.json");
   let date = new Date().toLocaleDateString();
   let time = new Date().toLocaleTimeString();
   let log = async (message) => {
-    let msg = await client.channels.get("287972594622267393").send(message);
-    client.user.setGame(`${msg.guildConf.prefix}help | v${pkg.version}`);
+    await client.channels.get("287972594622267393").send(message);
+    return client.user.setGame(`${settings.prefix}help | v${pkg.version}`);
   };
 
   let bootup = [
